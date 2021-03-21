c9e90f21501f83356adee305b3c608e4de24bff5
hodyy <pavel.hodyc@seznam.cz>
2021-03-21 18:50:44 +0100

    fix

--- Knihovna/src/mojeKnihovna/Admin.java
+++ Knihovna/src/mojeKnihovna/Admin.java
@@ -1,26 +1,36 @@
 package mojeKnihovna;
 
+import java.time.LocalDate;
 
 public class Admin extends User {
 	
 	
 	public Admin() {}
 	
-	public void borrowBook(){
-		
+	@Override
+	public void deleteBook(Database database) {
+		System.out.println("Zadej ID knihy, která se má odebrat:");	
+		database.deleteBookFromDatabase(scanner.readInt());
+		database.showDatabase();
+		// NEVIM PROÈ ALE MUSÍ TU BÝT TOTO JINAK BERE VSTUP PRO DALŠÍ OPERACI. PROÈ?????
+		scanner.readLine();
 	}
 	
-	public void returnBook() {
+	@Override
+	public void addBook(Database database) {
+		System.out.println("Zadej Název knihy");
+		String name = scanner.readLine();
+		System.out.println("Zadej Jmeno autora");
+		String autor = scanner.readLine();
+		System.out.println("Zadej datum vydání ve tvaru yyyy-mm-dd");
+		String date = scanner.readLine();
 		
-	}
-	
-	public void deleteBook() {
+		Book book=new Book(name, autor,LocalDate.parse(date));
+		database.addBookToDatabase(book);
 		
+		database.showDatabase();
+	
 	}
-	
-	
-	public void addBook() {
-	
 		
-	}
+	
 }
--- Knihovna/src/mojeKnihovna/Reader.java
+++ Knihovna/src/mojeKnihovna/Reader.java
@@ -2,24 +2,16 @@ package mojeKnihovna;
 
 public class Reader extends User {
 	SingleScanner scanner = SingleScanner.getInstanceOfScanner();
-	
+	@Override
 	public  void deleteBook(Database database) {	
 		System.out.println("!!!!! NELZE PROVÉST OPERACI V ROLI USER !!!!!\n---->Stiskni ENTER<----");
 		scanner.readLine();	
 	}
-	
+	@Override
 	public  void addBook(Database database) {	
 		System.out.println("!!!!! NELZE PROVÉST OPERACI V ROLI USER !!!!!\n---->Stiskni ENTER<----");
 		scanner.readLine();	
 	}
 	
-	public void borrowBook(){
-		
-	}
-	
-	public void returnBook() {
-		
-	}
-
 	
 }
